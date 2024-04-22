efc35d355292994b4a2de0d5dca1e683f1813b09
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2024-04-21 21:58:59 -0400

    add img a=to bannerabout section

--- src/Components/BannerAbout.jsx
+++ src/Components/BannerAbout.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import AboutBanner from '../assets/About/aboutbanner.png'
+import Imgig1 from '../assets/Posts/post3.svg'
 
 const BannerAbout = () => {
   return (
-    
+    <>
     <img className="banner_about" src={AboutBanner} />
+    <div class="row g-0">
+            <div class="col-lg-3 col-sm-6">
+                <div class="insta-post">
+                    <img className='imagen-bannerAbout' src={Imgig1} alt="" />
+                    <a href="https://www.instagram.com/p/CwVIDbDoN_Q/?igsh=MXdvaHB4OXV3dnU0aw%3D%3D" class="insta-btn">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                </div>
+            </div>
+           
+        </div>
+    
+    </>
   )
 }
 
