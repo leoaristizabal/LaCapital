9319e06dab9cbeab2f96e806983585db81a765ca
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2024-07-01 13:56:14 -0400

    add form logic with emailjs

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import emailjs from 'emailjs-com';
 
 const Form = () => {
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_j5gsehh', 'template_ym6ywef', e.target, 'cLgYA8En2L7izRjl7')
+          .then((result) => {
+              console.log(result.text);
+              alert('Mensaje enviado con éxito!');
+          }, (error) => {
+              console.log(error.text);
+              alert('Hubo un error al enviar el mensaje:', error.text);
+          });
+    
+        e.target.reset();
+      };
     return (
         <section id="reservation">
         <div class="container">
@@ -10,27 +25,27 @@ const Form = () => {
                     <p>¿Algún comentario o sugerencia?<br/> No dudes en escribirnos, estamos aquí para servirte y asegurarnos de que tu experiencia con La Capital Carniceria sea excepcional.</p>
                 </div>
             </div>
-            <form action="#" class="row justify-content-center">
+            <form onSubmit={sendEmail} class="row justify-content-center">
                 <div class="col-lg-8">
                     <div class="row g-3">
                         <div class="form-group col-md-6">
-                            <input type="text" class="form-control" placeholder="Nombre"/>
+                            <input type="text" class="form-control" placeholder="Nombre" name="from_name"/>
                         </div>
                         <div class="form-group col-md-6">
-                            <input type="email" class="form-control" placeholder="Email"/>
+                            <input type="email" class="form-control" placeholder="Email" name="email"/>
                         </div>
                         <div class="form-group col-md-6">
-                            <input type="text" class="form-control" placeholder="Fecha"/>
+                            <input type="date" class="form-control" placeholder="Fecha" name="date"/>
                         </div>
                         <div class="form-group col-md-6">
-                            <input type="text" class="form-control" placeholder="Numero Telefónico"/>
+                            <input type="tel" class="form-control" placeholder="Número Telefónico" name="tel"/>
                         </div>
                         <div class="form-group col-md-12">
-                            <textarea name="" id="" cols="30" rows="4" class="form-control"
+                            <textarea name="message" id="" cols="30" rows="4" class="form-control"
                                 placeholder="Mensaje"></textarea>
                         </div>
                         <div class="form-group col-md-12 text-center">
-                            <a href="#" class="btn btn-brand">Enviar Mensaje</a>
+                            <button type="submit" class="btn btn-brand">Enviar Mensaje</button>
                         </div>
                     </div>
                 </div>
